205f1a6bf77c07dd13f6a82a87e14c75c534077c
Love <Love@DESKTOP-CR6LECC>
2022-12-17 12:09:49 -0600

    Add project files.

---
+++ ChatAppGraphQl/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatAppGraphQl.Model {
+    public class User {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public DateTime LastSeen { get; set; }
+        public DateTime LastTyped { get; set; }
+    }
+}
